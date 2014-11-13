5b45278d44bb7de8afae63222dec98014741f1b3
Volodymyr Yurchenko <yuw726@gmail.com>
2014-11-13 15:00:39 +0200

    Cinder and nova config added

--- packstack/puppet/templates/cinder_ceph.pp
+++ packstack/puppet/templates/cinder_ceph.pp
@@ -368,3 +368,37 @@ exec { "iptables-save":
   command  => "/sbin/iptables-save > /etc/sysconfig/iptables",
   refreshonly => true,
 }
+
+
+
+cinder_config {
+  "DEFAULT/rbd_user":                           value => "volumes";
+  "DEFAULT/volume_driver":                      value => "cinder.volume.drivers.rbd.RBDDriver";
+  "DEFAULT/rbd_user":                           value => "volumes";
+  "DEFAULT/rbd_secret_uuid":                    value => "b83f6812-3bdc-8291-723f-2956749dfc7d"; # !!!
+  "DEFAULT/rbd_pool":                           value => "volumes";
+  "DEFAULT/rbd_ceph_conf":                      value => "/etc/ceph/ceph.conf";
+  "DEFAULT/rbd_flatten_volume_from_snapshot":   value => "false";
+  "DEFAULT/rbd_max_clone_depth":                value => "5";
+  
+  "DEFAULT/backup_driver":                      value => "cinder.backup.drivers.ceph";
+  "DEFAULT/backup_ceph_conf":                   value => "/etc/ceph/ceph.conf";
+  "DEFAULT/backup_ceph_user":                   value => "cinder-backup";
+  "DEFAULT/backup_ceph_pool":                   value => "backups";
+  "DEFAULT/backup_ceph_chunk_size":             value => "134217728";
+  "DEFAULT/backup_ceph_stripe_unit":            value => "0";
+  "DEFAULT/backup_ceph_stripe_count":           value => "0";
+  "DEFAULT/restore_discard_excess_bytes":       value => "true";
+}
+
+nova_config {
+  "DEFAULT/rbd_user":                           value => "volumes";
+  "DEFAULT/rbd_secret_uuid":                    value => "b83f6812-3bdc-8291-723f-2956749dfc7d"; # !!!
+  
+  "libvirt/libvirt_images_type":                value => "rbd";
+  "libvirt/libvirt_images_rbd_pool":            value => "volumes";
+  "libvirt/libvirt_images_rbd_ceph_conf":       value => "/etc/ceph/ceph.conf";
+  "libvirt/libvirt_inject_password":            value => "false";
+  "libvirt/libvirt_inject_key":                 value => "false";
+  "libvirt/libvirt_inject_partition":           value => "-2";
+}
